c052e95d60ad9018f27fd94822347872019a5732
LeidyAyalaSosa <104851448+LeidyAyalaSosa@users.noreply.github.com>
2022-07-28 05:40:27 -0500

    Update Tarea.js

--- src/components/Tarea.js
+++ src/components/Tarea.js
@@ -1,30 +1,26 @@
-import React, { useState } from "react"; //este es el componente tarea
+import React, { useState } from "react";
 import "../App.css";
 
 const Task = (props) => {
-  const [editMode, setEditMode] = useState(false); //estado es un buleano que es falso o verdadero segùn se le pulse
-  const [editText, setEditText] = useState(""); //estado que controla el input de editar
+  const [editMode, setEditMode] = useState(false);
+  const [editText, setEditText] = useState("");
 
   const edit = () => {
-    //funciòn para editar
     setEditMode(true);
   };
 
   const handleEdit = (event) => {
-    //funciòn manejar edit, evento
-    setEditText(event.target.value); //Establecer ediciòn de texto, evento, targeta y valor
+    setEditText(event.target.value);
   };
 
   const submitEdit = (event) => {
-    //enviar ediciòn, evento
-    event.preventDefault(); //evita que la app se actualice a cada momento
-    props.edit(props.id, editText); //propiedades de edit, las propiedades que se quieren editar
-    setEditText(""); //vaciar (o enviar) texto editado
-    setEditMode(false); //para que mande las tareas y no el formulario de editar
+    event.preventDefault();
+    props.edit(props.id, editText);
+    setEditText("");
+    setEditMode(false);
   };
 
   const deleteTask = () => {
-    //funciòn borrar tarea, se llaman las propiedades
     props.erase(props.id);
   };
 
@@ -60,9 +56,3 @@ const Task = (props) => {
 };
 
 export default Task;
-
-//esto es porque las tareas se van a pasar por props
-//{!editMode ? (se llama operador ternario) si se le da editar cambia el estado, si la tarea no se edita va a salir normal pero si sì se va a actualizar
-//<input value={editText onChange={handleEdit} en el input va el valor que es igual al editar texto y en cambio o en cambioe està manejar ediciòn
-
-//para hacer la check box creo que debe crearse el estado, la funciòn, el condicional y la funciòn que controle la checkbox
